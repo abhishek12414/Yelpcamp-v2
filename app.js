0aed7f1b5e6ff08b46de9d1caf8082406267300f
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-05-28 23:29:09 +0530

    implement store and fetch data from the database

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
+const express       = require('express'),
+      app           = express(),
+      mongoose      = require('mongoose'),
+      bodyParser    = require('body-parser');
 
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 
-let campgrounds = [
-    {name: 'Salmon Creek', image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx'},
-    {name: 'Granite Hill', image: 'http://www.nationalparks.nsw.gov.au/~/media/0C7C17503AAA45ECBE28979DDB954468.ashx'},
-    {name: "Mountain Goat's Rest", image: 'http://www.nationalparks.nsw.gov.au/~/media/1772ABCFF6644EE5A4C966E987F987BD.ashx'}
-];
+//SCHEMA SETUP
+const CmapgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model('Campground', CmapgroundSchema);
+
+//Create a campground
+// Campground.create(
+//     {
+//         name: 'Salmon Creek', 
+//         image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx'
+//     }, (err, campground)=>{
+//         if(err) {
+//             console.log('Error Occurs');
+//             console.log(err);
+//         } else {
+//             console.log('Data Inserted');
+//             console.log(campground);
+//         }
+//     });
+
+// let campgrounds = [
+    
+//     {name: 'Granite Hill', image: 'http://www.nationalparks.nsw.gov.au/~/media/0C7C17503AAA45ECBE28979DDB954468.ashx'},
+//     {name: "Mountain Goat's Rest", image: 'http://www.nationalparks.nsw.gov.au/~/media/1772ABCFF6644EE5A4C966E987F987BD.ashx'}
+// ];
 
 app.get("/", (req, res)=>{
     // res.send('This will be the landing page soon!')
@@ -23,13 +49,27 @@ app.post('/campgrounds', (req, res)=>{
         image: req.body.camp_image
     }
 
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+    //Create a new campground and save to database
+    Campground.create(newCampground, (err, campground)=>{
+        if(err)
+            console.log(err);
+        else
+            res.redirect('/campgrounds');
+    });
 });
 
 
 app.get('/campgrounds', (req, res)=> {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    // res.render('campgrounds', {campgrounds: campgrounds});
+    
+    // Get all campgrounds from the database
+    Campground.find({}, (err, allCampgrounds)=> {
+        if(err)
+            console.log(err);
+        else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 
